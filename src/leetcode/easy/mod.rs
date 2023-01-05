4803c9684c6f964a04c935ef20ccbb5a81dcb153
FluffyCorleone <illuminous9@gmail.com>
2023-01-05 18:57:36 -0500

    Initial commit

---
+++ src/leetcode/easy/mod.rs
@@ -0,0 +1,22 @@
+pub fn factorial( n:u32 ) -> u32 {
+    if n == 1 { 
+        return 1 ; 
+    } 
+
+    n * factorial( n - 1 )
+}
+
+// https://leetcode.com/problems/number-of-1-bits/
+
+pub fn hammingWeight (n: u32) -> i32 {
+
+    let nn = format!( "{n:b}" ) ;
+
+    nn.chars().fold( 0, |accum, value| {
+        if value == '1' {
+            accum + 1
+        } else {
+            accum
+        }
+    })    
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod leetcode ;
+
+use crate::leetcode::easy::{ factorial, hammingWeight } ;
+
+fn main() {
+
+    let a = hammingWeight( 454645645 ) ;
+
+    println!( "{a}" ) ;
+}
